77c0f36491427607562831981984898141155932
ckrum21 <108544020+ckrum21@users.noreply.github.com>
2022-08-03 13:56:44 -0400

    main function and pseudocode

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,17 @@
-// Assignment Code
+//Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+
+
+
+function generatePassword(){
+//1. Prompt user for the password
+    //a. password length 8 < 128 characters
+    //b. Lowercase, uppercase, numbers, and special characters
+//2. Validate the input
+//3. generate password based on criteria
+// display password to page
+}
 
 // Write password to the #password input
 function writePassword() {
